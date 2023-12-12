78b24e6738ec13d5000347dab5e311e7cba765c8
Krishna <mahato77@hotmail.com>
2023-12-12 10:01:42 +0100

    basic table without function

--- src/App.jsx
+++ src/App.jsx
@@ -8,39 +8,44 @@ import Table from "./components/Table";
 function App() {
   const [data, setData] = useState([]);
 
-  const getData = () => {
-    fetch("mock.json", {
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-    })
-      .then(function (response) {
-        console.log(response);
-        return response.json();
-      })
-      .then(function (myJson) {
-        console.log(myJson);
-        setData(myJson);
-      });
-  };
-
-  useEffect(() => {
-    getData();
-  }, []);
-
   return (
     <>
       <Navbar />
-      <h2>Inventory Dashboard</h2>
-      <Table data={data} />
-      <ul>
-        {data &&
-          data.length > 0 &&
-          data.map((item) => <li key={uuidv4()}>{item.Hersteller}</li>)}
-      </ul>
+      <div className="flex justify-center items-center flex-col mt-20">
+        <h4 className="font-bold text-gray-600">Inventory Records</h4>
+        <Table data={data} />
+      </div>
     </>
   );
 }
 
 export default App;
+
+// const getData = () => {
+//   fetch("mock.json", {
+//     headers: {
+//       "Content-Type": "application/json",
+//       Accept: "application/json",
+//     },
+//   })
+//     .then(function (response) {
+//       console.log(response);
+//       return response.json();
+//     })
+//     .then(function (myJson) {
+//       console.log(myJson);
+//       setData(myJson);
+//     });
+// };
+
+// useEffect(() => {
+//   getData();
+// }, []);
+
+{
+  /* <ul>
+        {data &&
+          data.length > 0 &&
+          data.map((item) => <li key={uuidv4()}>{item.Hersteller}</li>)}
+      </ul> */
+}
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,41 +1,44 @@
-import { v4 as uuidv4 } from 'uuid';
-
+import { v4 as uuidv4 } from "uuid";
 
 export default function Table({ data }) {
-// console.log("from table", data)
-
-
+  // console.log("from table", data);
 
   return (
     <div className="overflow-x-auto">
-       {/* {
-        data && data.length > 0 && data.map((item)=><li key={uuidv4()}>{item.Hersteller}</li>)
-        } */}
-        {data && data.map((item) => {
-            return (
-                <table className="table" key={uuidv4()}>
-                {/* head */}
-                <thead>
-                <tr key={uuidv4()}>
-                    <th>Benutzer</th>
-                    <th>Gerätename</th>
-                    <th>Hersteller</th>
-                </tr>
-                </thead>
-                <tbody>
-                {/* row 1 */}
-                <tr className="bg-base-200">
-                    <td>{item.Benutzer}</td>
-                    <td>{item.Gerätename}</td>
-                    <td>{item.Gerätename}</td>
-                </tr>
-                </tbody>
-            </table>
-            )
-        })
-
-        }
-  
+      <table className="table">
+        {/* head */}
+        <thead>
+          <tr>
+            <th></th>
+            <th>Benutzer</th>
+            <th>Gerätename</th>
+            <th>Hersteller</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* row 1 */}
+          <tr>
+            <th>1</th>
+            <td>Lange</td>
+            <td>EEG101</td>
+            <td>Microsoft</td>
+          </tr>
+          {/* row 2 */}
+          <tr>
+            <th>2</th>
+            <td>Koch</td>
+            <td>EEG117</td>
+            <td>Microsoft</td>
+          </tr>
+          {/* row 3 */}
+          <tr>
+            <th>3</th>
+            <td>Caesar</td>
+            <td>EEG119</td>
+            <td>Dell</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
-  )
+  );
 }
