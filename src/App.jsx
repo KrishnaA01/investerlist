3c51dc42dd5328ecddc6c5fca909e0be3a5dc489
Krishna <mahato77@hotmail.com>
2023-12-12 11:22:50 +0100

    got first functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,80 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Navbar from "./components/Navbar";
-import { v4 as uuidv4 } from "uuid";
 
 import "./App.css";
 import Table from "./components/Table";
 
 function App() {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState([
+    { id: 1, benutzer: "Lange", gerätename: "EEG076", hersteller: "Dell" },
+    { id: 2, benutzer: "Nsair", gerätename: "EEG104", hersteller: "Microsoft" },
+  ]);
+
+  const [editingRow, setEditingRow] = useState(null);
+
+  const [newRow, setNewRow] = useState({
+    benutzer: "",
+    gerätename: "",
+    hersteller: "",
+  });
+
+  const handleAddRow = () => {
+    setData([...data, { id: data.length + 1, ...newRow }]);
+    setNewRow({ benutzer: "", gerätename: "", hersteller: "" });
+  };
+
+  const handleDeleteRow = (id) => {
+    setData(data.filter((row) => row.id !== id));
+  };
+
+  const handleEditRow = (id) => {
+    setEditingRow(id);
+  };
+
+  const handleSaveEdit = (id, updatedData) => {
+    setData(
+      data.map((row) => (row.id === id ? { ...row, ...updatedData } : row))
+    );
+    setEditingRow(null);
+  };
 
   return (
     <>
       <Navbar />
       <div className="flex justify-center items-center flex-col mt-20">
         <h4 className="font-bold text-gray-600">Inventory Records</h4>
-        <Table data={data} />
+        <Table
+          data={data}
+          editingRow={editingRow}
+          handleDeleteRow={handleDeleteRow}
+          handleEditRow={handleEditRow}
+          handleSaveEdit={handleSaveEdit}
+        />
+        <div>
+          <input
+            placeholder="Benutzer"
+            value={newRow.benutzer}
+            onChange={(e) => setNewRow({ ...newRow, benutzer: e.target.value })}
+          />
+          <input
+            placeholder="Gerätename"
+            value={newRow.gerätename}
+            onChange={(e) =>
+              setNewRow({ ...newRow, gerätename: e.target.value })
+            }
+          />
+          <input
+            placeholder="Hersteller"
+            value={newRow.hersteller}
+            onChange={(e) =>
+              setNewRow({ ...newRow, hersteller: e.target.value })
+            }
+          />
+          <button onClick={handleAddRow}>Add Row</button>
+        </div>
       </div>
     </>
   );
 }
 
 export default App;
-
-// const getData = () => {
-//   fetch("mock.json", {
-//     headers: {
-//       "Content-Type": "application/json",
-//       Accept: "application/json",
-//     },
-//   })
-//     .then(function (response) {
-//       console.log(response);
-//       return response.json();
-//     })
-//     .then(function (myJson) {
-//       console.log(myJson);
-//       setData(myJson);
-//     });
-// };
-
-// useEffect(() => {
-//   getData();
-// }, []);
-
-{
-  /* <ul>
-        {data &&
-          data.length > 0 &&
-          data.map((item) => <li key={uuidv4()}>{item.Hersteller}</li>)}
-      </ul> */
-}
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,7 +1,11 @@
-import { v4 as uuidv4 } from "uuid";
-
-export default function Table({ data }) {
-  // console.log("from table", data);
+export default function Table({
+  data,
+  editingRow,
+  handleDeleteRow,
+  handleEditRow,
+  handleSaveEdit,
+}) {
+  console.log("from table", data);
 
   return (
     <div className="overflow-x-auto">
@@ -17,26 +21,54 @@ export default function Table({ data }) {
         </thead>
         <tbody>
           {/* row 1 */}
-          <tr>
-            <th>1</th>
-            <td>Lange</td>
-            <td>EEG101</td>
-            <td>Microsoft</td>
-          </tr>
-          {/* row 2 */}
-          <tr>
-            <th>2</th>
-            <td>Koch</td>
-            <td>EEG117</td>
-            <td>Microsoft</td>
-          </tr>
-          {/* row 3 */}
-          <tr>
-            <th>3</th>
-            <td>Caesar</td>
-            <td>EEG119</td>
-            <td>Dell</td>
-          </tr>
+          {data.map((row) => (
+            <tr key={row.id}>
+              <td>{row.id}</td>
+              <td>
+                {editingRow === row.id ? (
+                  <input value={row.benutzer} />
+                ) : (
+                  row.benutzer
+                )}
+              </td>
+              <td>
+                {editingRow === row.id ? (
+                  <input value={row.gerätename} />
+                ) : (
+                  row.gerätename
+                )}
+              </td>
+              <td>
+                {editingRow === row.id ? (
+                  <input value={row.hersteller} />
+                ) : (
+                  row.hersteller
+                )}
+              </td>
+              <td>
+                {editingRow === row.id ? (
+                  <button
+                    onClick={() =>
+                      handleSaveEdit(row.id, {
+                        benutzer: "updated",
+                        gerätename: "updated",
+                        hersteller: "updated",
+                      })
+                    }
+                  >
+                    Save
+                  </button>
+                ) : (
+                  <>
+                    <button onClick={() => handleEditRow(row.id)}>Edit</button>
+                    <button onClick={() => handleDeleteRow(row.id)}>
+                      Delete
+                    </button>
+                  </>
+                )}
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
