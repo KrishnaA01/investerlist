668190afe3c70491de9ca26a80e24f93243652a6
Krishna <mahato77@hotmail.com>
2023-12-14 17:34:59 +0100

    edit bug fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Navbar from "./components/Navbar";
-import Table from "./components/Table";
+// import Table from "./components/Table";
 
 import "./App.css";
 
@@ -9,9 +9,8 @@ function App() {
     { id: 1, benutzer: "Lange", gerätename: "EEG076", hersteller: "Dell" },
     { id: 2, benutzer: "Nsair", gerätename: "EEG104", hersteller: "Microsoft" },
     { id: 3, benutzer: "John", gerätename: "EEG151", hersteller: "Microsoft" },
+    { id: 4, benutzer: "Mike", gerätename: "EEG153", hersteller: "Lenovo" },
   ]);
-
-  const [editingRow, setEditingRow] = useState(null);
 
   const [newRow, setNewRow] = useState({
     benutzer: "",
@@ -19,43 +18,50 @@ function App() {
     hersteller: "",
   });
 
-  // handles add row into table
+  const [editingRow, setEditingRow] = useState(null);
+
   const handleAddRow = () => {
     setData([...data, { id: data.length + 1, ...newRow }]);
     setNewRow({ benutzer: "", gerätename: "", hersteller: "" });
   };
 
-  //handles deleting
   const handleDeleteRow = (id) => {
     setData(data.filter((row) => row.id !== id));
   };
 
   const handleEditRow = (id) => {
     setEditingRow(id);
-    console.log("handledit=>", id);
   };
 
-  //handles edited info
-  const handleSaveEdit = (id, updatedData) => {
-    setData(
-      data.map((row) => (row.id === id ? { ...row, ...updatedData } : row))
+  const handleSaveEdit = (id) => {
+    setData((prevData) =>
+      prevData.map((row) =>
+        row.id === id
+          ? {
+              ...row,
+              benutzer: editingRow === id ? newRow.benutzer : row.benutzer,
+              gerätename:
+                editingRow === id ? newRow.gerätename : row.gerätename,
+              hersteller:
+                editingRow === id ? newRow.hersteller : row.hersteller,
+            }
+          : row
+      )
     );
     setEditingRow(null);
+    setNewRow({ benutzer: "", gerätename: "", hersteller: "" });
   };
 
   return (
     <>
       <Navbar />
       <div className="flex justify-center items-center flex-col mt-20">
-        <h4 className="font-bold text-gray-600 text-lg">Inventory Records</h4>
-        <Table
-          data={data}
-          editingRow={editingRow}
-          handleDeleteRow={handleDeleteRow}
-          handleEditRow={handleEditRow}
-          handleSaveEdit={handleSaveEdit}
-        />
         <div>
+          <h1 className="font-bold text-gray-600 text-2xl mb-10">
+            Inventarlisten
+          </h1>
+        </div>
+        <div className="mb-10">
           <input
             className="input input-bordered mr-2"
             placeholder="Benutzer"
@@ -81,6 +87,87 @@ function App() {
           <button className="btn btn-outline btn-info" onClick={handleAddRow}>
             Add Row
           </button>
+        </div>
+        {/* data table */}
+        <div className="overflow-x-auto">
+          <table className="table text-lg">
+            <thead>
+              <tr className="text-lg">
+                <th></th>
+                <th>Benutzer</th>
+                <th>Gerätename</th>
+                <th>Hersteller</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((row) => (
+                <tr key={row.id}>
+                  <td>{row.id}</td>
+                  <td>
+                    {editingRow === row.id ? (
+                      <input
+                        value={newRow.benutzer}
+                        onChange={(e) =>
+                          setNewRow({ ...newRow, benutzer: e.target.value })
+                        }
+                      />
+                    ) : (
+                      row.benutzer
+                    )}
+                  </td>
+                  <td>
+                    {editingRow === row.id ? (
+                      <input
+                        value={newRow.gerätename}
+                        onChange={(e) =>
+                          setNewRow({ ...newRow, gerätename: e.target.value })
+                        }
+                      />
+                    ) : (
+                      row.gerätename
+                    )}
+                  </td>
+                  <td>
+                    {editingRow === row.id ? (
+                      <input
+                        value={newRow.hersteller}
+                        onChange={(e) =>
+                          setNewRow({ ...newRow, hersteller: e.target.value })
+                        }
+                      />
+                    ) : (
+                      row.hersteller
+                    )}
+                  </td>
+                  <td>
+                    {editingRow === row.id ? (
+                      <button
+                        className="btn btn-outline btn-info"
+                        onClick={() => handleSaveEdit(row.id)}
+                      >
+                        Save
+                      </button>
+                    ) : (
+                      <>
+                        <button
+                          className="mr-2 btn btn-outline btn-info"
+                          onClick={() => handleEditRow(row.id)}
+                        >
+                          Edit
+                        </button>
+                        <button
+                          className="btn btn-outline btn-secondary"
+                          onClick={() => handleDeleteRow(row.id)}
+                        >
+                          X
+                        </button>
+                      </>
+                    )}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       </div>
     </>
